85592ae6b763ab6cd6452f981992ccb09631819c
Shanoj <shanojtr84@gmail.com>
2018-04-24 23:05:35 +1000

    modi

--- src/main/java/auto/common/InitAppium2.java
+++ src/main/java/auto/common/InitAppium2.java
@@ -236,10 +236,10 @@ public class InitAppium2 {
 			capabilities.setCapability("udid", UDID);
 			capabilities.setCapability("wdaLocalPort", WDAPortNum);
 			capabilities.setCapability("fullReset", false);
-//		     capabilities.setCapability("xcodeOrgId", "UCV7WN4XD7");
-//			 capabilities.setCapability("xcodeSigningId", "iPhone Developer");
-			capabilities.setCapability("keychainPath", "/Users/shanoj/Library/Keychains/MyKeychain.keychain-db");
-			capabilities.setCapability("keychainPassword", "test123");
+		     capabilities.setCapability("xcodeOrgId", "UCV7WN4XD7");
+			 capabilities.setCapability("xcodeSigningId", "iPhone Developer");
+//			capabilities.setCapability("keychainPath", "/Users/shanoj/Library/Keychains/MyKeychain.keychain-db");
+//			capabilities.setCapability("keychainPassword", "test123");
 			 capabilities.setCapability("showXcodeLog", true);
 	
 			driver = new IOSDriver(new URL("http://0.0.0.0:" + port + "/wd/hub"), capabilities);
